05c1d47f904a744fbe9a6d126ac5f23b045f86a8
Matthew Crouse <mcrouse7531@gmail.com>
2025-03-14 20:18:10 -0400

    VER 0.1

--- mattcrouse/src/App.js
+++ mattcrouse/src/App.js
@@ -1,4 +1,5 @@
 import logo from './Baller.jpg';
+import bodey from './phone.png';
 import './App.css';
 
 function App(props) {
@@ -7,20 +8,25 @@ function App(props) {
 
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>{name}</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <p>
+            baller
+          </p>
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://www.youtube.com/watch?v=xFYQQPAOz7Y&ab_channel=EminemMusic"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Lose Yourself
+          </a>
+          <iframe className='youtube' src="https://www.youtube.com/embed/jQAl4kUFdOg" title="Video offers shocking glimpse into flooded Richmond Water Treatment Plant" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <img src={bodey} className='bodey'/>
+        </header>
+        <div className="background">
+        </div>
     </div>
   );
 }
